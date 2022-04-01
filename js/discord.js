3897b698839dc32d3c932911d093c19a1aaec584
GamingBox <gamingbox156@gmail.com>
2022-04-01 19:22:51 +0200

    Discord page improved

---
+++ js/discord.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("WoenselRP").onclick = function(){
+    location.href = "https://discord.gg/UguCzA4AYR"
+}
+document.getElementById("WoenselOnderwereld").onclick = function(){
+    location.href = "https://discord.gg/8ASR8JrE8U"
+}
+document.getElementById("WoenselSupport").onclick = function(){
+    location.href = "https://discord.gg/rAMW2xqa8A"
+}
