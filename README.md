5d9929597093e784ba00f162d47699b3e9ef61f6
GamingBox <gamingbox156@gmail.com>
2022-04-04 16:14:01 +0200

    donations added

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the source-cpode of the official Woensel website
--- js/discord.js
+++ js/discord.js
@@ -9,3 +9,6 @@ document.getElementById("WoenselOnderwereld").onclick = function(){
 document.getElementById("WoenselSupport").onclick = function(){
     location.href = "https://discord.gg/cYH2xAbTY7"
 }
+document.getElementById("WoenselCombat").onclick = function(){
+    location.href = "https://discord.gg/9BEPQAHXr9"
+}
