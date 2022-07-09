fd4e03753aff79fdbfdcad99ec42fd3b42d4d7bd
GamingBox <gamingbox156@gmail.com>
2022-07-09 11:55:05 +0200

    fixed links

--- js/discord.js
+++ js/discord.js
@@ -1,14 +1,7 @@
 
-
-document.getElementById("WoenselRP").onclick = function(){
-    location.href = "https://discord.gg/pqrY38hPeu"
-}
-document.getElementById("WoenselOnderwereld").onclick = function(){
-    location.href = "https://discord.gg/ZtNXcrS3Fh"
-}
-document.getElementById("WoenselSupport").onclick = function(){
+document.getElementById("WoenselSupport").onclick = function () {
     location.href = "https://discord.gg/cYH2xAbTY7"
 }
-document.getElementById("WoenselCombat").onclick = function(){
+document.getElementById("WoenselCombat").onclick = function () {
     location.href = "https://discord.gg/9BEPQAHXr9"
 }
